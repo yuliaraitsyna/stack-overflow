d189734617c8066c6d122b6d698b55104be3f6a4
yuliaraitsyna <yuliaraitsyna@gmail.com>
2025-01-16 17:15:52 +0300

    feat: implement responsive form and add error boundary

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,17 +1,20 @@
+import './styles/colors.css'
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes } from 'react-router-dom'
-
 import { routes } from './routes/routes'
-
-import './styles/colors.css'
+import { ErrorBoundary } from 'react-error-boundary'
+import { ErrorPage } from '../pages/ErrorPage/ErrorPage'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        {routes}
-      </Routes>
-    </BrowserRouter>
+    <ErrorBoundary FallbackComponent={ErrorPage}>
+      <BrowserRouter>
+        <Routes>
+          {routes}
+        </Routes>
+      </BrowserRouter>
+    </ErrorBoundary>
   </StrictMode>,
 )
--- src/features/AuthForm/AuthForm.tsx
+++ src/features/AuthForm/AuthForm.tsx
@@ -1,45 +1,97 @@
-import styles from './AuthForm.module.css'
-
-import { Button, TextField } from '@mui/material'
-import  AuthFormProps from './AuthForm.types'
+import styles from './AuthForm.module.css';
+import { Button, TextField, Typography } from '@mui/material';
 import { useAuth } from '../../app/hooks/useAuth';
-import { useRef } from 'react';
+import { useForm } from 'react-hook-form';
+import AuthFormProps from './AuthForm.types';
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
 
-const AuthForm: React.FC<AuthFormProps> = ({type}) => {
-    const formText = type === 'login' ? "Login" : "Register";
-    const usernameInput = useRef<HTMLInputElement>(null);
-    const passwordInput = useRef<HTMLInputElement>(null);
-    const confirmPasswordInput = useRef<HTMLInputElement>(null);
-
-    const {login, register} = useAuth();
-
-    const handleFormSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-
-        const username = usernameInput.current?.value || '';
-        const password = passwordInput.current?.value || '';
-
-        if (type === 'login') {
-            login(username, password);
-        } else {
-            const confirmPassword = confirmPasswordInput.current?.value || '';
-            if (password !== confirmPassword) {
-                alert('Passwords do not match');
-                return;
-            }
-            register(username, password);
-        }
-    };
-
-    return (
-        <form className={styles.form} method='POST' onSubmit={handleFormSubmit}>
-            <h1>{formText}</h1>
-            <TextField ref={usernameInput} label="username"></TextField>
-            <TextField ref={passwordInput} label="password"></TextField>
-            { type === 'register' && <TextField ref={confirmPasswordInput} label="confirm password"></TextField> }
-            <Button type='submit' variant='contained'>{formText}</Button>
-        </form>
-    )
+interface FormInputs {
+  username: string;
+  password: string;
+  confirmPassword?: string;
 }
 
-export { AuthForm }
+const AuthForm: React.FC<AuthFormProps> = ({ type }) => {
+  const { login, register: registerUser } = useAuth();
+  const [errorMessage, setErrorMessage] = useState<string>('');
+  const navigate = useNavigate();
+
+  const formText = type === 'login' ? 'Login' : 'Register';
+  const buttonText = type === 'login' ? "Register" : "Login";
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormInputs>();
+
+  const onSubmit = async (data: FormInputs) => {
+    const { username, password, confirmPassword } = data;
+
+    if (type === 'login') {
+        login(username, password).catch((error) => setErrorMessage(error.message));
+      } else {
+        if (password !== confirmPassword) {
+          throw new Error("Passwords don't match");
+        }
+        registerUser(username, password).catch((error) => setErrorMessage(error.message));
+    }
+  };
+
+  const handleFormTypeChange = () => {
+    if(type === 'login') {
+        navigate('/register');
+    }
+    else {
+        navigate('/login');
+    }
+  }
+
+  return (
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+      <Typography variant="h4" gutterBottom>{formText}</Typography>
+      <TextField
+        {...register('username', { required: 'Username is required' })}
+        label="username"
+        type="text"
+        fullWidth
+        error={!!errors.username}
+        helperText={errors.username?.message}
+        required
+      />
+      <TextField
+        {...register('password', {
+          required: 'Password is required',
+          minLength: { value: 5, message: 'Password must be at least 5 characters' },
+        })}
+        label="Password"
+        type="password"
+        fullWidth
+        error={!!errors.password}
+        helperText={errors.password?.message}
+        required
+      />
+      {type === 'register' && (
+        <TextField
+          {...register('confirmPassword', {
+            required: 'Please confirm your password',
+            validate: (value) => value === watch('password') || "Passwords don't match",
+          })}
+          label="Confirm Password"
+          type="password"
+          fullWidth
+          error={!!errors.confirmPassword}
+          helperText={errors.confirmPassword?.message}
+          required
+        />
+      )}
+      <Typography color='error'>{errorMessage}</Typography>
+      <Button type="submit" variant="contained" color="primary">{formText}</Button>
+      <Button className={styles.navigateBtn} variant='text' onClick={handleFormTypeChange}>{buttonText}</Button>
+    </form>
+  );
+};
+
+export { AuthForm };
