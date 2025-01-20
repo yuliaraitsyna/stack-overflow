e620b935812d1f6e9fbee1b11a53ed9872f55614
yuliaraitsyna <yuliaraitsyna@gmail.com>
2025-01-20 14:27:24 +0300

    feat: implement pagination

--- src/widgets/SnippetList/SnippetList.tsx
+++ src/widgets/SnippetList/SnippetList.tsx
@@ -1,16 +1,24 @@
+import styles from './SnippetList.module.css';
+
 import { Snippet } from "../Snippet/Snippet";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { useGetSnippetsQuery } from "../../app/redux/api/snippetsApi";
-import { setSnippets } from "../../app/redux/slice/snippetSlice";
+import { setCurrentPage, setLimit, setSnippets } from "../../app/redux/slice/snippetSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../app/redux/store/store";
-import { CircularProgress } from "@mui/material";
+import { Box, CircularProgress, Pagination } from "@mui/material";
+import { LimitButtons } from "../../features/LimitButtons/LimitButtons";
 import { v4 as uuidv4 } from 'uuid'
 
 const SnippetList = () => {
-    const { data: fetchedSnippets, isLoading } = useGetSnippetsQuery();
-    const dispatch = useDispatch();
     const snippets = useSelector((state: RootState) => state.snippets.snippets);
+    const totalPages = useSelector((state: RootState) => state.snippets.totalPages);
+    const currentPage = useSelector((state: RootState) => state.snippets.currentPage);
+    const limit = useSelector((state: RootState) => state.snippets.limit)
+
+    const { data: fetchedSnippets, isLoading } = useGetSnippetsQuery({page: currentPage, limit});
+
+    const dispatch = useDispatch();
 
     useEffect(() => {
         if (fetchedSnippets) {
@@ -18,8 +26,20 @@ const SnippetList = () => {
         }
     }, [fetchedSnippets, dispatch]);
 
+    const handlePageChange = (event: React.ChangeEvent<unknown>, value: number) => {
+        dispatch(setCurrentPage(value));
+    }
+
+    const handleLimitChange = (limit: number) => {
+        dispatch(setLimit(limit));
+    }
+
     return (
         <>
+            <Box className={styles.pagination}>
+                <LimitButtons onLimitChange={handleLimitChange}></LimitButtons>
+            </Box>
+            <Pagination count={totalPages} page={currentPage} onChange={handlePageChange}/>
             {
                 isLoading 
                 ? 
