2c4feed378a3eeaf67ee39e6e7c3e518aa529578
yuliaraitsyna <yuliaraitsyna@gmail.com>
2025-01-20 12:31:46 +0300

    feat: implement snippet list with store

--- src/widgets/SnippetList/SnippetList.tsx
+++ src/widgets/SnippetList/SnippetList.tsx
@@ -4,6 +4,8 @@ import { useGetSnippetsQuery } from "../../app/redux/api/snippetsApi";
 import { setSnippets } from "../../app/redux/slice/snippetSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../app/redux/store/store";
+import { CircularProgress } from "@mui/material";
+import { v4 as uuidv4 } from 'uuid'
 
 const SnippetList = () => {
     const { data: fetchedSnippets, isLoading } = useGetSnippetsQuery();
@@ -11,17 +13,20 @@ const SnippetList = () => {
     const snippets = useSelector((state: RootState) => state.snippets.snippets);
 
     useEffect(() => {
-        if (fetchedSnippets && Array.isArray(fetchedSnippets)) {
+        if (fetchedSnippets) {
             dispatch(setSnippets(fetchedSnippets));
         }
     }, [fetchedSnippets, dispatch]);
 
     return (
         <>
-            {!isLoading && Array.isArray(snippets) && snippets.map(snippet => (
-                <Snippet key={snippet.id} snippet={snippet} />
-            ))}
-            {isLoading && <p>Loading...</p>}
+            {
+                isLoading 
+                ? 
+                <CircularProgress /> 
+                : 
+                snippets.map(snippet => <Snippet key={uuidv4()} snippet={snippet} />)
+            }
         </>
     );
 };
