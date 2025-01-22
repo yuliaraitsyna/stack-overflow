4b164a7016639bb0e29d6fe6e836a52ceda15751
yuliaraitsyna <yuliaraitsyna@gmail.com>
2025-01-22 18:21:48 +0300

    feat: add edit widget UI

--- src/pages/UserPage/UserPage.tsx
+++ src/pages/UserPage/UserPage.tsx
@@ -5,6 +5,7 @@ import { UserWidget } from "../../widgets/UserWidget/UserWidget";
 import { useSelector } from 'react-redux';
 import { RootState } from '../../app/redux/store/store';
 import { UserWelcomer } from '../../features/UserWelcomer/UserWelcomer';
+import { EditWidget } from '../../widgets/EditWidget/EditWidget';
 
 const UserPage = () => {
     const user = useSelector((state: RootState) => state.auth.user);
@@ -14,6 +15,7 @@ const UserPage = () => {
             <Box className={styles.container}>
                 <UserWelcomer user={user}/>
                 {user && <UserWidget user={user}/>}
+                <EditWidget />
             </Box>
         </>
     )
