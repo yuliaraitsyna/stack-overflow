8747d147e37f62cd946ddc655ff45d7246a229aa
yuliaraitsyna <yuliaraitsyna@gmail.com>
2025-01-23 12:24:51 +0300

    feat: implement changing password

---
+++ src/app/redux/api/authApi.ts
@@ -0,0 +1,55 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { StatisticResponse } from "./authApi.types";
+
+export const authApi = createApi({
+  reducerPath: "authApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "/api" }),
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: ({ username, password }: { username: string; password: string }) => ({
+        url: "/auth/login",
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: { username, password },
+      }),
+    }),
+    register: builder.mutation({
+      query: ({ username, password }: { username: string; password: string }) => ({
+        url: "/register",
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: { username, password },
+      }),
+    }),
+    logout: builder.mutation<void, void>({
+        query: () => ({
+            url: '/auth/logout',
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+        })
+    }),
+    deleteUser: builder.mutation<void, number>({
+        query: (id) => ({
+          url: `/users/${id}`,
+          method: 'DELETE',
+        }),
+    }),
+    changePassword: builder.mutation({
+        query: ({oldPassword, newPassword} : {oldPassword: string, newPassword: string}) => ({
+          url: '/me/password',
+          method: 'PATCH',
+          headers: {"Content-Type": "application/json"},
+          body: {oldPassword, newPassword},
+        }),
+    }),
+    getStatistic: builder.query<StatisticResponse, number>({
+      query: (id: number) => ({
+          url: `/users/${id}/statistic`,
+          method: 'GET',
+          headers: { "Content-Type": "application/json" },
+      })
+    })
+  }),
+});
+
+export const { useLoginMutation, useRegisterMutation, useLogoutMutation, useGetStatisticQuery, useDeleteUserMutation, useChangePasswordMutation } = authApi;
---
+++ src/features/ChangePasswordForm/ChangePasswordForm.tsx
@@ -0,0 +1,84 @@
+import styles from './ChangePasswordForm.module.css';
+
+import { Box, Button, TextField, Typography } from "@mui/material";
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import { useChangePasswordMutation } from '../../app/redux/api/authApi';
+
+interface ChangePasswordFormInputs {
+    oldPassword: string,
+    newPassword: string,
+    confirmPassword: string
+}
+
+const ChangePasswordForm = () => {
+    const {t} = useTranslation();
+    const [errorMessage, setErrorMessage] = useState('');
+    const [changePassword] = useChangePasswordMutation();
+
+    const {
+        register: formRegister,
+        handleSubmit,
+        watch,
+        formState: {errors},
+    } = useForm<ChangePasswordFormInputs>();
+
+    const onSubmit = async (data: ChangePasswordFormInputs) => {
+        const {oldPassword, newPassword} = data;
+
+        try {
+            await changePassword({oldPassword, newPassword})
+            .unwrap()
+            .catch(error => setErrorMessage(error.message));
+
+            //modal window
+        }
+        catch(error) {
+            console.error(error);
+        }
+    }
+
+    return (
+        <Box className={styles.container}>
+            <Typography variant='body2'>Change your password: </Typography>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <TextField 
+                    {...formRegister('oldPassword', {required: 'Old password is required'})}
+                    label={t('oldPassword')}
+                    type='password'
+                    error={!!errors.oldPassword}
+                    helperText={errors.oldPassword?.message}
+                />
+                <TextField
+                    {...formRegister('newPassword', 
+                        {
+                            required: 'New password is required',
+                            minLength: {value: 5, message: 'Password be at least 5 characters'},
+                        }
+                    )}
+                    label={t('newPassword')}
+                    type='password'
+                    error={!!errors.newPassword}
+                    helperText={errors.newPassword?.message}
+                />
+                <TextField
+                    {...formRegister('confirmPassword', 
+                        {
+                            required: 'Confirm password is required',
+                            validate: (value) => value === watch('newPassword') || "Password don't match",
+                        }
+                    )}
+                    label={t('confirmPassword')}
+                    type='password'
+                    error={!!errors.confirmPassword}
+                    helperText={errors.confirmPassword?.message}
+                />
+                <Typography color='error'>{errorMessage}</Typography>
+                <Button color='success' variant='contained' type='submit'>Change password</Button>
+            </form>
+        </Box>
+    )
+}
+
+export { ChangePasswordForm };
