ea14181aa64e2c7d4d0392f81a8ad2652583e9c3
yuliaraitsyna <yuliaraitsyna@gmail.com>
2025-01-27 14:01:47 +0300

    feat: add show code button

--- src/features/Question/Question.tsx
+++ src/features/Question/Question.tsx
@@ -1,15 +1,29 @@
 import styles from './Question.module.css';
 
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 import CodeMirror from "@uiw/react-codemirror";
 import { QuestionHeader } from "../QuestionHeader/QuestionHeader";
 import { QuestionProps } from "./Question.types";
 import { Box } from "@mui/material";
+import { useState } from 'react';
 
 const Question: React.FC<QuestionProps> = ({question}) => {
+    const [isShownCode, setIsShownCode] = useState(false);
+    
+    const handleToggleCode = () => {
+        setIsShownCode(prev => !prev);
+    }
+    
     return (
         <Box className={styles.question}>
             <QuestionHeader question={question} />
-            {question.attachedCode && <CodeMirror value={question.attachedCode} height='50px' readOnly />}
+            {
+                question.attachedCode && 
+                <>
+                    <RemoveRedEyeIcon color='primary' onClick={handleToggleCode} />
+                    {isShownCode && <CodeMirror value={question.attachedCode} height='50px' readOnly />}
+                </>    
+            }
         </Box>
     )
 }
