5f98e5019750d16de982937cc3753b57f82bb3c7
yuliaraitsyna <yuliaraitsyna@gmail.com>
2025-01-30 12:23:49 +0300

    feat: implement change username feature

---
+++ src/app/redux/slices/authSlice/authSlice.ts
@@ -0,0 +1,36 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { User } from "../../../../entities/User/User";
+import { AuthState } from "./authSlice.types";
+import { StatisticResponse } from "../../api/authApi/authApi.types";
+
+const initialState: AuthState = {
+  user: null,
+  statistic: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setUser(state, action: PayloadAction<User>) {
+      state.user = action.payload;
+    },
+    updateUsername(state, action: PayloadAction<string>) {
+      if(state.user)
+        state.user.username = action.payload;
+      else
+        throw new Error('User is not defined');
+    },
+    logout(state) {
+      state.user = null;
+    },
+    setStatistics(state, action: PayloadAction<StatisticResponse>) {
+      state.statistic = action.payload.data.statistic;
+    },
+  },
+});
+
+const authReducer = authSlice.reducer;
+
+export const { setUser, logout, setStatistics, updateUsername } = authSlice.actions;
+export default authReducer;
