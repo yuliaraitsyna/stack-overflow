6229a36ca50948f53aecdc94a2838744c8b4b29b
yuliaraitsyna <yuliaraitsyna@gmail.com>
2025-01-30 12:07:23 +0300

    feat: allow info modal to disapear

--- src/features/InfoModal/InfoModal.tsx
+++ src/features/InfoModal/InfoModal.tsx
@@ -1,18 +1,28 @@
 import styles from './InfoModal.module.css';
-
+import { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 import { InfoModalProps } from "./InfoModal.types";
 import { Alert, Collapse } from "@mui/material";
 
-const InfoModal: React.FC<InfoModalProps> = ({message, type, open}) => {
+const InfoModal: React.FC<InfoModalProps> = ({ message, type, open }) => {
+    const [visible, setVisible] = useState(open);
+
+    useEffect(() => {
+        if (open) {
+            setVisible(true);
+            const timer = setTimeout(() => setVisible(false), 3000);
+            return () => clearTimeout(timer);
+        }
+    }, [open]);
+
     return createPortal(
-        <Collapse in={open}>
+        <Collapse in={visible}>
             <Alert severity={type} className={styles.alert}>
                 {message}
             </Alert>
         </Collapse>,
         document.body
-    )
+    );
 }
 
 export { InfoModal };
