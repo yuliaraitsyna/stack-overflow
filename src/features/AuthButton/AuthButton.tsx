b33527e2a2b51a4b1ab6d5945647c3e1337d7dcc
yuliaraitsyna <yuliaraitsyna@gmail.com>
2025-01-31 20:32:03 +0300

    fix: fix logout

--- src/features/AuthButton/AuthButton.tsx
+++ src/features/AuthButton/AuthButton.tsx
@@ -7,6 +7,7 @@ import { useTranslation } from 'react-i18next';
 import { useLogoutMutation } from '../../app/redux/api/authApi/authApi';
 import { useEffect, useState } from 'react';
 import { userSelector } from '../../app/redux/selectors/authSelectors';
+import { InfoModal } from '../InfoModal/InfoModal';
 
 const AuthButton = () => {
     const {t} = useTranslation();
@@ -15,26 +16,41 @@ const AuthButton = () => {
     const dispatch = useDispatch();
     const user = useSelector(userSelector);
     const [buttonText, setButtonText] = useState(t('signIn'));
+    const [errorMessage, setErrorMessage] = useState<string>('');
 
     useEffect(() => {
         setButtonText(user ? t('signOut') : t('signIn'));
     }, [t, user]);
 
-    const handleClick = () => {
-        if(user) {
-            logout();
-            dispatch(storeLogout());
-            navigate('/');
+    const handleClick = async () => {
+        try {
+            if(user) {
+                await logout()
+                .then(() => {
+                    dispatch(storeLogout());
+                    navigate('/');
+                })
+                .catch(error => { throw new Error(error.message)})
+            }
+            else {
+                navigate('/login');
+            }
         }
-        else {
-            navigate('/login');
+        catch(error) {
+            if(error instanceof Error) {
+                setErrorMessage(error.message);
+            }
+            else setErrorMessage("Something went wrong");
         }
     }
 
     return (
-        <button className={styles.button} onClick={handleClick}>
-            {buttonText}
-        </button>
+        <>
+            <button className={styles.button} onClick={handleClick}>
+                {buttonText}
+            </button>
+            <InfoModal type='error' message={errorMessage} onClose={() => setErrorMessage('')} open={!!errorMessage} />
+        </>
     )
 }
 
